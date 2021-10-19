7e9436a17489681a23d8ea6f51d173e04ee19ca7
Csyyang <693765678@qq.com>
2021-10-19 14:10:39 +0800

    add

---
+++ pages/discovery/discovery.js
@@ -0,0 +1,25 @@
+
+import { getData2 } from '../../utils/util.js'
+
+Page({
+    data: {
+        tabList: ['推荐', '圆桌', '热门', '收藏'],
+        isActive: 0,
+        images: ['../../images/24213.jpg', '../../images/24280.jpg', '../../images/1444983318907-_DSC1826.jpg'],
+        feed: []
+    },
+    changeAC(e) {
+        this.setData({
+            isActive: e.currentTarget.dataset.index
+        })
+    },
+    onLoad() {
+        this.getData()
+    },
+    getData() {
+        const feed = getData2()
+        this.setData({
+            feed: feed,
+        })
+    },
+})
